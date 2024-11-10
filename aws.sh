d891e4bdea742c1ec205df98b0918cd26753113e
AliFareed0009 <alifareed0009@gmail.com>
2024-11-10 15:37:39 +0500

    removing aws.sh form main

--- aws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# First install AWS-CLI in your system
-# Visit the below link and follow the instructions to install https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-# Visit AWS CLI Reference and search for the commands you want to use https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
-
-# This is used for AWS EC2 Instances listing
-aws ec2  describe-instances
